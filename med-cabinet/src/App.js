6a6054d4427aac88038e5436045b77fb144ad1a4
Chris Forrest <redrider_cr@yahoo.com>
2020-04-29 13:48:19 -0400

    it's working

--- med-cabinet/src/App.js
+++ med-cabinet/src/App.js
@@ -3,8 +3,10 @@ import { Switch, Route } from 'react-router-dom'
 import SignupForm from './components/signup-form'
 import PrivateRoute from './components/PrivateRoute'
 import UserPage from './components/UserPage'
+import SignInForm from './components/signInForm'
 import './App.css'
 import './components/signup-form'
+import './components/signInForm'
 
 function App() {
   return (
@@ -14,8 +16,12 @@ function App() {
         <PrivateRoute exact path={'/userpage'} component={UserPage}/>
         <Route exact path={'/userpage-test'} component={UserPage}/>
         <Route path={'/signup-form'}>
+       
           <h1>Welcome to the app.</h1>
           <SignupForm />
+           </Route>
+         <Route path={'/signInForm'}>
+          <SignInForm />
         </Route>
       </Switch>
     </div>
