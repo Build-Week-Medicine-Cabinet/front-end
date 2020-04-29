103e55cd51a809e735c069cfbbf929fdceb29268
Chris Forrest <redrider_cr@yahoo.com>
2020-04-28 21:55:51 -0400

    no styling and axios calls aren't working

--- med-cabinet/src/components/userInputForm.js
+++ med-cabinet/src/components/userInputForm.js
@@ -46,7 +46,8 @@ const formValidation = yup.object().shape({
         anxiety: string(), 
         headaches: string(), 
         fatigue: string(),
-    },
+    }
+    .required("at least one aliment is required"),
     effect: yup
     .string(),
     flavor: yup
@@ -56,24 +57,139 @@ const formValidation = yup.object().shape({
 
 export default function UserInputForm () {
 
+    const[userCard, setUserCard] = useState([])
+    const[formValues, setFormValues] = useState(initialFormValues)
+    const[formDisabled, setFormDisabled] = useState(true)
+    const[formErrors, setFormErrors] = useState(initialFormErrors)
 
+    const getUserCard = () => {
+        axios.get(url)
+        .then(res => {
+            setUserCard(res.data)
+        })
+        .catch( err => {
+            console.log('err')
+        })
+    }
+
+    useEffect(() => {
+        getUserCard()
+    }, [])
+
+    const postUserCard = card => {
+        axios.post(url, card)
+        .then(res => {
+            setUserCard([res.data, ...userCard])
+        })
+        .catch( err => {
+            console.log('err')
+        })
+    }
+
+    useEffect(() => {
+        formValidation.isValid(formValues)
+        .then(valid => {
+            setFormDisabled(!valid)
+        })
+    },[formValues])
+
+    const onSubmit = evt => {
+        evt.preventDefault()
+
+        const newUserCard = {
+            ailments: object.keys(formValues.ailments)
+            .filter(ailments => formValues.ailments [ailment] === true)
+        }
+        postUserCard(newUserCard)
+        setFormValues(initialFormValues)
+    }
+
+
+    const onInputChange = evt => {
+        const name = evt.target.name 
+        const value = evt.target.name
+
+        yup
+            .reach(formValidation, name)
+            .validate(value)
+            .then(valid => {
+                setFormErrors({
+                    ...formErrors,
+                    [name]: '',
+                })
+            })
+
+            .catch(err => {
+                setFormErrors({
+                    ...formErrors,
+                    [name]: er.errors[0]
+                })
+            })
+
+            setFormValues({
+                ...formValues,
+                [name]: value,
+            })
+    }
+
+    const onCheckboxChange = evt => {
+        const { name } = evt.target
+        const isChecked = evt.target.isChecked
+
+        setFormValues({
+            ...formValues,
+            ailments:{
+                ...formValues.ailments,[name]: isChecked,
+            }
+        })
+    }
+
+    
     return(
         <form>
         <h2>Ailments</h2>
-        <label>depression</label><input type="checkbox" name="depression"/>
-        <labe>inflammation</labe><input type="checkbox" name="inflammation"/>
-        <labe>insomnia</labe><input type="checkbox" name="insomnia"/>
-        <labe>lack of appetite</labe><input type="checkbox" name="lackOfAppetite"/>
-        <labe>muscle spasms</labe><input type="checkbox" name="muscleSpasms"/>
-        <labe>nausea</labe><input type="checkbox" name="nausea"/>
-        <labe>pain</labe><input type="checkbox" name="pain"/>
-        <labe>seizures</labe><input type="checkbox" name="seizures"/>
-        <labe>stress</labe><input type="checkbox" name="stress"/>
-        <labe>anxiety</labe><input type="checkbox" name="anxiety"/> 
-        <labe>headaches</labe><input type="checkbox" name="headaches"/>
-        <labe>fatigue</labe><input type="checkbox" name="fatigue"/>
-        <label>effect</label><input type="text" name="effect"/>
-        <label>flavor</label><input type="text" name="flavor"/>
+        <label>depression</label><input type="checkbox" name="depression"
+        checked={formValues.ailments.depression} onCheckboxChange={onCheckboxChange}/>
+
+        <labe>inflammation</labe><input type="checkbox" name="inflammation"
+         checked={formValues.ailments.inflammation} onCheckboxChange={onCheckboxChange}/>
+
+        <labe>insomnia</labe><input type="checkbox" name="insomnia"
+         checked={formValues.ailments.insomnia} onCheckboxChange={onCheckboxChange}/>
+
+        <labe>lack of appetite</labe><input type="checkbox" name="lackOfAppetite"
+         checked={formValues.ailments.lackOfAppetite} onCheckboxChange={onCheckboxChange}/>
+
+        <labe>muscle spasms</labe><input type="checkbox" name="muscleSpasms"
+         checked={formValues.ailments.muscleSpasms} onCheckboxChange={onCheckboxChange}/>
+
+        <labe>nausea</labe><input type="checkbox" name="nausea"
+         checked={formValues.ailments.nausea} onCheckboxChange={onCheckboxChange}/>
+
+        <labe>pain</labe><input type="checkbox" name="pain"
+         checked={formValues.ailments.pain} onCheckboxChange={onCheckboxChange}/>
+
+        <labe>seizures</labe><input type="checkbox" name="seizures"
+         checked={formValues.ailments.seizures} onCheckboxChange={onCheckboxChange}/>
+
+        <labe>stress</labe><input type="checkbox" name="stress"
+         checked={formValues.ailments.stress} onCheckboxChange={onCheckboxChange}/>
+
+        <labe>anxiety</labe><input type="checkbox" name="anxiety"
+         checked={formValues.ailments.anxiety} onCheckboxChange={onCheckboxChange}/>
+
+        <labe>headaches</labe><input type="checkbox" name="headaches"
+         checked={formValues.ailments.headaches} onCheckboxChange={onCheckboxChange}/>
+
+        <labe>fatigue</labe><input type="checkbox" name="fatigue"
+         checked={formValues.ailments.fatigue} onCheckboxChange={onCheckboxChange}/>
+
+        <label>effect</label><input type="text" name="effect" 
+        values={formValues.effect} onChange={onInputChange}/>
+        
+        <label>flavor</label><input type="text" name="flavor"
+        values={formValues.flavor} onChange={onInputChange}/>
+        <button onClick={onSubmit} disabled={disabled}>submit</button>
         </form>
     )
 }
