be4e88f24e2aec7465e10c59a6673541b23d86e3
Chris Forrest <redrider_cr@yahoo.com>
2020-04-29 11:23:52 -0400

    fixed yup string error had to add yup.string()

--- med-cabinet/src/components/userInputForm.js
+++ med-cabinet/src/components/userInputForm.js
@@ -34,18 +34,18 @@ const initialFormErrors = {
 /***************form validation not sure how to make just on of the ailments required ******************/
 const formValidation = yup.object().shape({
     ailments:{
-        depression: string(), 
-        inflammation: string(), 
-        insomnia: string(), 
-        lackOfAppetite: string(),
-        muscleSpasms: string(), 
-        nausea: string(), 
-        pain: string(), 
-        seizures: string(), 
-        stress: string(),
-        anxiety: string(), 
-        headaches: string(), 
-        fatigue: string(),
+        depression: yup.string(), 
+        inflammation: yup.string(), 
+        insomnia: yup.string(), 
+        lackOfAppetite: yup.string(),
+        muscleSpasms: yup.string(), 
+        nausea: yup.string(), 
+        pain: yup.string(), 
+        seizures: yup.string(), 
+        stress: yup.string(),
+        anxiety: yup.string(), 
+        headaches: yup.string(), 
+        fatigue: yup.string(),
     }
     .required("at least one aliment is required"),
     effect: yup
@@ -97,7 +97,7 @@ export default function UserInputForm () {
         evt.preventDefault()
 
         const newUserCard = {
-            ailments: object.keys(formValues.ailments)
+            ailments: Object.keys(formValues.ailments)
             .filter(ailments => formValues.ailments [ailment] === true)
         }
         postUserCard(newUserCard)
@@ -122,7 +122,7 @@ export default function UserInputForm () {
             .catch(err => {
                 setFormErrors({
                     ...formErrors,
-                    [name]: er.errors[0]
+                    [name]: err.errors[0]
                 })
             })
 
@@ -147,6 +147,9 @@ export default function UserInputForm () {
     
     return(
         <form>
+            <div>
+                {formErrors.ailments}
+            </div>
         <h2>Ailments</h2>
         <label>depression</label><input type="checkbox" name="depression"
         checked={formValues.ailments.depression} onCheckboxChange={onCheckboxChange}/>
@@ -189,7 +192,7 @@ export default function UserInputForm () {
         
         <label>flavor</label><input type="text" name="flavor"
         values={formValues.flavor} onChange={onInputChange}/>
-        <button onClick={onSubmit} disabled={disabled}>submit</button>
+        <button onClick={onSubmit} disabled={formDisabled}>submit</button>
         </form>
     )
 }
