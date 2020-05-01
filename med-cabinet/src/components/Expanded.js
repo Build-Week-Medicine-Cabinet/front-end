c5a16a37b29797ae6da6854ecf531b56d95b9f6b
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-05-01 07:56:13 -0700

    complete-functionality-juan-madero

--- med-cabinet/src/components/Expanded.js
+++ med-cabinet/src/components/Expanded.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { useHistory } from 'react-router-dom'
+import { Card, StyledButton } from './styledComponents'
+import styled from 'styled-components'
 
 const Expanded = () => {
     const expandedData = useSelector(state => state.expandedData)
-    
 
     // send user to find seller
     const findSellers = strainName => {
@@ -13,32 +14,40 @@ const Expanded = () => {
         window.location.replace(`https://www.leafly.com/strains/${formattedStrainName}/availability`)
     }
 
+    const Container = styled.div`
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    `
+
     return (
-        <div>
+        <Container>
+            <Card>
+                <h2>The Details!</h2>
+                <ul>
+                    <li>Searched Symptoms: {expandedData.symptoms.join(', ')}</li>
+                    <li>Searched Flavors: {expandedData.flavor.join(', ')}</li>
+                    <li>Searched Effects: {expandedData.effect.join(', ')}</li>
+                    
+                </ul>
+            </Card>
             
-            <h2>The Details!</h2>
-            <ul>
-                <li>Searched Symptoms: {expandedData.symptoms.join(', ')}</li>
-                <li>Searched Flavors: {expandedData.flavor.join(', ')}</li>
-                <li>Searched Effects: {expandedData.effect.join(', ')}</li>
-                
-            </ul>
             <h3>Recommended Strains</h3>
             {expandedData.results.map((result, index) => {
                 return (
-                    <div className='strainContainer' key={index+1}>
-                        <h4>#{index+1} Strain: {result.strain}</h4>
+                    <Card className='strainContainer' key={index+1}>
+                        <h3>#{index+1} Strain: {result.strain}</h3>
                         <p>Type: {result.type}</p>
                         <p>Rating: {result.rating}</p>
                         <p>Effects: {result.effects.join(', ')}</p>
                         <p>Flavors: {result.flavor.join(', ')}</p>
-                        <h6>Description</h6>
+                        <h4>Description</h4>
                         <p>{result.description}</p>
-                        <button onClick={() => findSellers(result.strain)}>Find Local Sellers</button>
-                    </div>
+                        <StyledButton onClick={() => findSellers(result.strain)}>Find Local Sellers</StyledButton>
+                    </Card>
                 )
             })}
-        </div>
+        </Container>
     )
 }
 
